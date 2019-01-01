f3b1995b063898c7ac7fc71907459f52946810d0
lei.fan <lei.fan@openjawtech.com>
2019-01-01 17:56:34 +0800

    Initial commit

---
+++ src/main/java/io/gihub/ericfan/springwsfb2g/SpringWsFb2gApplication.java
@@ -0,0 +1,14 @@
+package io.gihub.ericfan.springwsfb2g;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWsFb2gApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWsFb2gApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/io/gihub/ericfan/springwsfb2g/SpringWsFb2gApplicationTests.java
@@ -0,0 +1,17 @@
+package io.gihub.ericfan.springwsfb2g;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringWsFb2gApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
